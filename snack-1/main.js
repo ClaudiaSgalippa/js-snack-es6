86996705677b0a434617caf78cd602bac07b8009
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-09 16:30:07 +0200

    Crezione objects

---
+++ snack-1/main.js
@@ -0,0 +1,28 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+const bicycles = [
+
+    { 
+        "nome": "Bianchi",
+        "peso": "6.6",
+    }
+
+    { 
+        "nome": "Pinarello",
+        "peso": "7.5",
+    }
+
+    { 
+        "nome": "Colnago",
+        "peso": "10",
+    }
+
+    { 
+        "nome": "Canyon",
+        "peso": "12.5",
+    }
+
+]
