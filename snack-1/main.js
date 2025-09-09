8daa676a9f46e8d0677b6090c982f12705ce75bf
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-09 17:18:33 +0200

    Completamento primo esercizio

--- snack-1/main.js
+++ snack-1/main.js
@@ -3,28 +3,47 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
 
+//-- ARRAY WITH OBJECTS --//
 const bicycles = [
 
     { 
-        "nome": "Bianchi",
-        "peso": "6.6",
-    },
-
-    { 
-        "nome": "Pinarello",
-        "peso": "7.5",
-    },
-
-    { 
         "nome": "Colnago",
-        "peso": "10",
+        "peso": 10,
+    },
+
+    { 
+        "nome": "Bianchi",
+        "peso": 6.6,
     },
 
     { 
         "nome": "Canyon",
-        "peso": "12.5",
+        "peso": 12.5,
+    },
+    
+    { 
+        "nome": "Pinarello",
+        "peso": 7.5,
     }
+    
+];
 
-]
+//for (let key in bicycles) { //Stampa in console dell'array con gli objects, just for check
+//    console.log(bicycles[key]);
+//}
 
-console.log(bicycles);
+//-- RICERCA DEL PESO MINORE CON IL CICLO FOR IN--//
+
+let lightestBicycle = bicycles [0]; 
+
+for (let i in bicycles) { 
+    if (bicycles[i].peso < lightestBicycle.peso) {
+        lightestBicycle = bicycles[i];
+    }
+}
+
+//--STAMPA PESO MINORE IN CONSOLE--//
+
+console.log("La bici dal peso più basso è la", lightestBicycle);
+
+
